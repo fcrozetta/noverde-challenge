232233efd35030a2ccc30785443f2fe405fe327e
Fernando Crozetta <fernando@czetta.com>
2020-08-20 05:36:13 -0300

    added http_util to the program

--- 1.lambdas/noverde-loan-id-get/noverde-loan-id-get.py
+++ 1.lambdas/noverde-loan-id-get/noverde-loan-id-get.py
@@ -1,6 +1,6 @@
 import json
 from loanRequestsDb import LoanRquestsDb
-
+from http_util.httpResponse import Response
 def lambda_handler(event, context):
     '''
     Handles the incomming event from API gateway
@@ -10,44 +10,30 @@ def lambda_handler(event, context):
         id = event['pathParameters']['id']
     except KeyError:
         #! Not being called via API gateway
-        return {
-            'statusCode': 400
-        }
+        return Response.Response(400)
 
-    return {
-        "statusCode":200,
-        'body': json.dumps(getLoanResponse(id)),
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*'
-        },
-        
-    }
+    customer = getLoanResponse(id)
+    return Response.Response(200,getLoanResponse(id))
 
 
 def getLoanResponse(id: str) -> dict:
     '''
-    Return the response from the DB
+    Return the altered response from DB
     '''
-    # Sanity check
-    if id is None:
-        return {}
 
     db = LoanRquestsDb()
-    db.checkRequest(id)
+    customer = db.checkRequest(id)
 
-    print(item)
+    # ? There should be a mapper to use dynamoDB json objects directly
+    # ? For now, doing it manually is acceptable
     response = {
-        "id":item['id'],
-        "status":item['status'],
-        "amount":float(item['amount']),
-        "terms": int(item['terms']),
+        "id":customer['id'],
+        "status":customer['status'],
+        "amount":float(customer['amount']),
+        "terms": int(customer['terms']),
     }
     if response['status'] == 'completed':
-        response["result"] = item['result']
-        
-        
+        response["result"] = customer['result']
         if response['result'] == 'refused':
-            response['refused_policy'] = item['refused_policy']
+            response['refused_policy'] = customer['refused_policy']
     return response
