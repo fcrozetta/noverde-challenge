7943f46f2ef05f17972244e8b439f0981e18567c
Fernando Crozetta <fernando@czetta.com>
2020-08-20 05:28:43 -0300

    implemented http class to handle CORS

--- 1.lambdas/noverde-loan-post/noverde-loan-post.py
+++ 1.lambdas/noverde-loan-post/noverde-loan-post.py
@@ -1,31 +1,18 @@
 import json
 from loanRequestsDb import LoanRequestsDb
+from http_util.httpResponse import Response
 def lambda_handler(event, context):
     '''
     Handles the incomming event from API gateway
     '''
-    # TODO : Create User based on parameters from body
 
     customer = json.loads(event['body'])
     db = LoanRequestsDb()
 
-    # Sanity check (But the validation happens at AWS API Gateway)
+    # Sanity check (The body validation happens at AWS API Gateway)
     if customer is None:
-        return {
-            'statusCode': 400
-        }
+        return Response.Response(400)
 
     id = db.addCustomer(customer)
 
-    # Return includes headers to avoid problems with CORS
-    return {
-        'statusCode': 200,
-        'body': json.dumps({"id":id}),
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*'
-        },
-    }
-
-
+    return Response.Response(200,{'id' : id})
